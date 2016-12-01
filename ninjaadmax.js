9321da50c0e2bacbf1269ec02966e2176c77f512
LAVA <ikechanneru@gmail.com>
2016-12-01 18:01:51 +0900

    Update ninjaadmax.js

--- ninjaadmax.js
+++ ninjaadmax.js
@@ -1,1 +1,1 @@
-document.write("<script src="http://adm.shinobi.jp/s/ac98ee5447737acd66f3abf9f3fde1a2"></script>");
+document.write("<script src="//adm.shinobi.jp/s/ac98ee5447737acd66f3abf9f3fde1a2"></script>");
